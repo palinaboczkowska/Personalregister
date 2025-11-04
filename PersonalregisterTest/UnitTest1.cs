2cfbaed5da476a136bc0863fec33753eb9d16ace
Palina Boczkowska <palina.boczkowska@gmail.com>
2025-11-04 16:22:50 +0100

    Har laht en enhetstest

---
+++ PersonalregisterTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+public class EmployeeTests
+{
+    [Fact]
+    public void Employee_ShouldStoreNameAndSalary()
+    {
+        var employee = new Employee { Name = "Anna", Salary = 25000 };
+
+        Assert.Equal("Anna", employee.Name);
+        Assert.Equal(25000, employee.Salary);
+    }
+ 
+
+}
