34c3a3ba1b22aa822d430a5ae21c0d1680d4cba6
Palina Boczkowska <palina.boczkowska@gmail.com>
2025-11-04 14:43:11 +0100

    Har lagt till 3 alternativ i registret

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -1,10 +1,80 @@
-﻿namespace Personalregister
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+﻿
+
+class Program {
+    static List<Employee> employeeRegister = new List<Employee>();
+
+    static void Main() {
+
+        bool isRunning = true;
+
+        while (isRunning) {
+            Console.WriteLine("`\nVälj ett alternativ: ");
+            Console.WriteLine("1: Lägg till anställd");
+            Console.WriteLine("2: Visa alla anställda och deras lön: ");
+            Console.WriteLine("3: Avsluta");
+
+            string choice = Console.ReadLine();
+            switch (choice) {
+                case "1":
+                    AddEmployee();
+                    break;
+                case "2":
+                    ShowEployees();
+                    break;
+                case "3":
+                    isRunning = false;
+                    break;
+
+            
+            
+            }
+        
         }
+    
     }
+
+    private static void ShowEployees()
+    {
+        if (employeeRegister.Count == 0) {
+            Console.WriteLine("Inga anställda registrerade.");
+            return;
+        }
+
+        Console.WriteLine("\nAnställdaregister: ");
+        foreach (Employee emp in employeeRegister) { 
+        emp.PrintInfo();
+        }
+
+    }
+
+    private static void AddEmployee()
+    {
+        Console.WriteLine("Ange namn: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Ange lön: ");
+        decimal salary = decimal.Parse(Console.ReadLine());
+        Employee newEmployee = new Employee {Name = name, Salary = salary };
+        employeeRegister.Add(newEmployee);
+        Console.WriteLine("Anställd " + name + " har lagts till.");
+
+    }
+}
+
+public class Employee {
+
+  public string Name {
+        get;
+        set;
+    }
+
+    public decimal Salary {
+        get;
+        set;
+    }
+
+    public void PrintInfo() {
+        Console.WriteLine($"Name: {Name}, Salary: {Salary} SEK");
+    }
+
 }
